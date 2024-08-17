f606803db8ae6f00ee2789ea7b6a36bcbe44dae8
d0stoievskiii <gcantarino@id.uff.br>
2024-08-17 16:55:01 -0300

    1st

---
+++ src/mousecontroller.cpp
@@ -0,0 +1,41 @@
+#include "mousecontroller.h"
+
+BOOLEAN WINAPI mouseController::UnInit() {
+	ZeroMemory(NtUserSendInput_Bytes, 30);
+
+	return TRUE;
+}
+
+BOOLEAN WINAPI mouseController::NtUserSendInput(UINT cInputs, LPINPUT pInputs, int cbSize) {
+	LPVOID NtUserSendInput_Spoof = VirtualAlloc(0, 0x1000, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+	if (!NtUserSendInput_Spoof) return FALSE;
+
+	memcpy(NtUserSendInput_Spoof, NtUserSendInput_Bytes, 30);
+	NTSTATUS result = reinterpret_cast<NTSTATUS(NTAPI*)(UINT, LPINPUT, int)>(NtUserSendInput_Spoof)(cInputs, pInputs, cbSize);
+
+	ZeroMemory(NtUserSendInput_Spoof, 0x1000);
+	VirtualFree(NtUserSendInput_Spoof, 0, MEM_RELEASE);
+	return (result > 0);
+}
+
+BOOLEAN WINAPI mouseController::moveAbs(int x, int y) {
+	INPUT input;
+	input.type = INPUT_MOUSE;
+	input.mi.mouseData = 0;
+	input.mi.time = 0;
+	input.mi.dx = x * (65536 / GetSystemMetrics(SM_CXSCREEN));
+	input.mi.dy = y * (65536 / GetSystemMetrics(SM_CYSCREEN));
+	input.mi.dwFlags = MOUSEEVENTF_MOVE | MOUSEEVENTF_VIRTUALDESK | MOUSEEVENTF_ABSOLUTE;
+	return NtUserSendInput(1, &input, sizeof(input));
+}
+
+BOOLEAN WINAPI mouseController::moveRel(int x, int y) {
+	INPUT input;
+	input.type = INPUT_MOUSE;
+	input.mi.mouseData = 0;
+	input.mi.time = 0;
+	input.mi.dx = x;
+	input.mi.dy = y;
+	input.mi.dwFlags = MOUSEEVENTF_MOVE | MOUSEEVENTF_VIRTUALDESK ;
+	return NtUserSendInput(1, &input, sizeof(input));
+}
---
+++ src/mousecontroller.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <windows.h>
+
+class mouseController {
+public:
+	BYTE NtUserSendInput_Bytes[30];
+	//BYTE NtUserGetAsyncKeyState_Bytes[30];
+
+	mouseController() {
+		LPVOID NtUserSendInput_address = GetProcAddress(GetModuleHandle(L"win32u"), "NtUserSendInput");
+		if (!NtUserSendInput_address) {
+
+			NtUserSendInput_address = GetProcAddress(GetModuleHandle(L"user32"), "NtUserSendInput");
+			if (!NtUserSendInput_address) {
+				//windows 7 or older
+				NtUserSendInput_address = GetProcAddress(GetModuleHandle(L"user32"), "SendInput");
+			}
+		}
+
+		memcpy(NtUserSendInput_Bytes, NtUserSendInput_address, 30);
+	}
+
+	BOOLEAN WINAPI UnInit();
+
+	BOOLEAN WINAPI NtUserSendInput(UINT cInputs, LPINPUT pInputs, int cbSize);
+
+	//UINT WINAPI NtUserGetAsyncKeyState(UINT Key);
+
+	BOOLEAN WINAPI moveAbs(int x, int y);
+	BOOLEAN WINAPI moveRel(int x, int y);
+
+	//UINT WINAPI GetAsyncKeyState(UINT Key);
+
+};
